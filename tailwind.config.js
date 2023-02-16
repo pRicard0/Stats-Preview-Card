20b3a9386554890d09fe52c19073befa5b0c394c
pRicard0 <pauloricardo.jesus2011@gmail.com>
2023-02-15 21:41:34 -0300

    colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+      },
+    },
+    fontSize: {
+      base: '0.9375rem',
+    },
+    fontFamily: {
+      'Inter': ['Inter', 'sans-serif'],
+      'Deca': ['Lexend Deca', 'sans-serif'],
+    }
   },
   plugins: [],
 }
