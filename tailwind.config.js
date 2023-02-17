51fd2b58267b63adbcd9e66ff89ae287d3d01579
pRicard0 <pauloricardo.jesus2011@gmail.com>
2023-02-17 18:54:04 -0300

    Finishing the responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,19 @@ module.exports = {
         'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
         'soft-violet': 'hsl(277, 64%, 61%)',
       },
+      spacing: {
+        'main': '68.75rem',
+        'section': '34.375rem',
+        'section-h': '28.125rem',
+      }
     },
     fontSize: {
       base: '0.9375rem',
       title: '2.2rem',
-      numbers: '1.75rem',
+      mobileTitle: '1.7rem',
+      numbers: '1.6rem',
+      mobileNumbers: '1.5rem',
+      mobileBase: '0.85rem',
     },
     fontFamily: {
       'Inter': ['Inter', 'sans-serif'],
